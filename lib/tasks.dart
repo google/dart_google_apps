a9edf536ef7ad0524b320e1daee7bfed3737f324
Cole Gerdemann <corvettecole@gmail.com>
2020-04-17 22:16:45 -0400

    Add tasks.dart to interface with Google Tasks

---
+++ lib/tasks.dart
@@ -0,0 +1,167 @@
+@JS('Tasks')
+library tasks;
+
+import 'package:js/js.dart';
+
+// https://developers.google.com/tasks/v1/reference/tasklists
+@JS()
+class Tasklists {
+  external String get kind;
+
+  external String get etag;
+
+  external String get nextPageToken;
+
+  external List<Tasklist> get items;
+
+  external static Tasklists list([num maxResults, String pageToken]);
+
+  external static Tasklist get(String tasklist);
+
+  external static Tasklist insert(Tasklist resource);
+
+  external static Tasklist update(Tasklist resource, String tasklist);
+
+  external static void remove(String id);
+}
+
+
+@JS()
+class Tasklist {
+  external String get etag;
+
+  external set etag(String etag);
+
+  external String get id;
+
+  external set id(String id);
+
+  external String get kind;
+
+  external set kind(String kind);
+
+  external String get selfLink;
+
+  external set selfLink(String selfLink);
+
+  external String get title;
+
+  external set title(String title);
+
+
+  external String get updated;
+
+  external set updated(String updated);
+
+}
+
+// https://developers.google.com/tasks/v1/reference/tasks
+@JS()
+class Tasks {
+  external String get etag;
+
+  external String get kind;
+
+  external String get nextPageToken;
+
+  external List<Task> get items;
+
+  external static void clear(String tasklist);
+
+  external static Task get(String tasklist, String task);
+
+  external static Task insert(Task resource, String tasklist,
+      [String parent, String previous]);
+
+  external static Tasks list(String tasklist,
+      [String completedMax,
+        String completedMin,
+        String dueMax,
+        String dueMin,
+        num maxResults,
+        String pageToken,
+        bool showCompleted,
+        bool showDeleted,
+        bool showHidden,
+        String updatedMin]);
+
+  external static Task move(String tasklist, String task,
+      [String parent, String previous]);
+
+  external static Task patch(Task resource, String tasklist, String task);
+
+  external static void remove(String tasklist, String task);
+
+  external static Task update(Task resource, String tasklist, String task);
+}
+
+@JS()
+class Task {
+  external String get etag;
+
+  external set etag(String etag);
+
+  external String get kind;
+
+  external set kind(String kind);
+
+  external String get id;
+
+  external set id(String id);
+
+  external String get title;
+
+  external set title(String title);
+
+  external String get parent;
+
+  external set parent(String parent);
+
+  external String get position;
+
+  external set position(String position);
+
+  external String get notes;
+
+  external set notes(String notes);
+
+  external String get status;
+
+  external set status(String status);
+
+  external bool get deleted;
+
+  external set deleted(bool deleted);
+
+  external bool get hidden;
+
+  external List<TaskLinks> get links;
+
+  external String get selfLink;
+
+  external set selfLink(String selfLink);
+
+
+  external String get due;
+
+  external set due(String due);
+
+
+  external String get completed;
+
+  external set completed(String completed);
+
+  external String get updated;
+
+  external set updated(String updated);
+
+}
+
+@JS()
+class TaskLinks {
+  external String get description;
+
+  external String get link;
+
+  external String get type;
+}
